b25709d899d0897a35a3f3f669a2d566515bbd07
209155548 <lour_s@outlook.com>
2025-11-19 17:24:32 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,14 @@
+
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    return re.findall(r'[A-Z][a-z0-9]*', formula)
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+   mport re
+
+def split_at_first_digit(formula):
+    match = re.search(r'\d', formula)
+    if not match:
+        return formula, ""  # אם אין ספרות
+    idx = match.start()
+    return formula[:idx], formula[idx:]
