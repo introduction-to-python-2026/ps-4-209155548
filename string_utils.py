14b0e84a8db26c0933184435d1523d525112acf4
209155548 <lour_s@outlook.com>
2025-11-19 17:26:18 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,8 +3,6 @@ def split_before_each_uppercases(formula):
     return re.findall(r'[A-Z][a-z0-9]*', formula)
 
 
-def split_at_first_digit(formula):
-   mport re
 
 def split_at_first_digit(formula):
     match = re.search(r'\d', formula)
