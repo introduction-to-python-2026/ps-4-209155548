1011111949c344123c9f2577be346e748a548fd6
209155548 <lour_s@outlook.com>
2025-11-19 17:35:14 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,12 +1,25 @@
 
 def split_before_each_uppercases(formula):
-    return re.findall(r'[A-Z][a-z0-9]*', formula)
-
+    start = 0
+    end = 1
+    elements_lst = []
+    
+    if not formula:
+        return elements_lst
+    
+    while end < len(formula):
+        if formula[end].isupper():
+            elements_lst.append(formula[start:end])
+            start = end
+        end+=1  
+        
+    elements_lst.append(formula[start:])
+    
+    return elements_lst
 
 
 def split_at_first_digit(formula):
-    match = re.search(r'\d', formula)
-    if not match:
-        return formula, ""  # אם אין ספרות
-    idx = match.start()
-    return formula[:idx], formula[idx:]
+    for char_index, char in enumerate(formula):
+        if char.isdigit():
+            return formula[:char_index], int(formula[char_index:])
+    return formula, 1
